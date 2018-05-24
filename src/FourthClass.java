4d707965c466ba9666b3b0e9a78c9565ac91068e
Neehad <nfattah@iconectiv.com>
2018-05-24 14:05:18 -0400

    Second commit to git witout first push

--- src/FourthClass.java
+++ src/FourthClass.java
@@ -9,6 +9,9 @@ public class FourthClass {
 		System.out.println("This is the fourth class we created");
 		System.out.println("Created on Version 1.5 pf HeadSecondThird");
 		
+		//added this statement right after committing to git but didnt push due to errors
+		System.out.println("Github 1st method without first push");
+		
 		boolean me = true;
 	}
 
